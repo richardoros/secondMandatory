103046a4acaca74add1b15b92d8065bf2039f880
richard.oros <richardoros@Richards-MacBook-Pro.local>
2017-05-19 15:08:26 +0200

    Initial Commit

---
+++ secondMandatoryTests/secondMandatoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  secondMandatoryTests.swift
+//  secondMandatoryTests
+//
+//  Created by Richard Oros on 19/05/2017.
+//  Copyright © 2017 Richard and Casian. All rights reserved.
+//
+
+import XCTest
+@testable import secondMandatory
+
+class secondMandatoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
